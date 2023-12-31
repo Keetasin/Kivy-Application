b4897c30eb40103e8d3df9ac5f87ab4f956fd6b0
Keetasin Kongsee <keetasin01596@gmail.com>
2023-12-31 13:54:56 +0700

    define self variable

--- SnakeGame.py
+++ SnakeGame.py
@@ -6,9 +6,15 @@ from kivy.core.window import Window
 
 class SnakeGame(Widget):
     def __init__(self, *args, **kwargs):
-        super(SnakeGame, self).__init__(**kwargs)
-    pass
 
+        super(SnakeGame, self).__init__(**kwargs)
+        self.snake_size = 20
+        self.snake_pos = [(100, 100), (100, 80), (100, 60)]
+        self.food_pos = (Window.width // 2, Window.height // 2)
+
+        self.direction = 'up'
+        self.snake_speed = 12
+   
 
 class SnakeApp(App):
     def build(self):
